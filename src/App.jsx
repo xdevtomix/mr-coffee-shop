74d4c6e31e1573e2df927d8b0d91fea0d06cc954
tomi <dev.dreissiger@gmail.com>
2021-11-12 16:49:15 +0100

    header in

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import styled, { StyleSheetManager } from "styled-components";
+
+import Header from "./components/Header";
+
 export default function App() {
   return (
-    <div data-component="app">
-      app
-    </div>
+    <StyleSheetManager disableVendorPrefixes>
+      <Container data-component="app">
+        <Header />
+      </Container>
+    </StyleSheetManager>
   )
 }
+
+const Container = styled.div`
+
+`;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default function Header() {
+    return (
+        <Container data-component="header">
+            header
+            <box-icon type='solid' name='hand'></box-icon>
+        </Container>
+    );
+}
+
+const Container = styled.header`
+
+`;
